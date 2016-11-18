f7fa10337d44715df622a9a49d8596e002761e73
Hai H Nguyen <nguyenhohai88@gmail.com>
2016-11-17 23:22:27 -0500

    Add files via upload

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Vui.MvcApplication" Language="C#" %>
---
+++ Startup.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin;
+using Owin;
+using Vui.Models;
+
+[assembly: OwinStartupAttribute(typeof(Vui.Startup))]
+namespace Vui
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+        private void createRoleAndUsers()
+        {
+            ApplicationDbContext context = new ApplicationDbContext();
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
+            var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+            if (!roleManager.RoleExists("Admin"))
+            {
+                var role = new IdentityRole();
+                role.Name = "Admin";
+                roleManager.Create(role);
+
+                var user = new ApplicationUser();
+                user.UserName = "Admin";
+                user.Email = "nguyenhohai88@gmail.com";
+                string password = "H@1D3pZ@i";
+                var chkUser = userManager.Create(user, password);
+                if (chkUser.Succeeded)
+                {
+                    var result = userManager.AddToRole(user.Id, "Admin");
+                }
+
+            }
+            
+            if (!roleManager.RoleExists("Mod"))
+            {
+                var role = new IdentityRole();
+                role.Name = "Mod";
+                roleManager.Create(role);
+           }
+
+            
+            if (!roleManager.RoleExists("User"))
+            {
+                var role = new IdentityRole();
+                role.Name = "User";
+                roleManager.Create(role);
+            }
+
+        }
+    }
+}
